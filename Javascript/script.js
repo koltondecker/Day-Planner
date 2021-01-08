f0a828f42d133b1220dc7e4c9d3f37ae60285df2
Kolton Decker <deckerman25@gmail.com>
2021-01-08 15:49:29 -0600

    Added submit button functionality that saves input text in that time slot to local storage as an object specific to that time slot.

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 
 const currentDate = moment().format("dddd, MMMM Do, YYYY");
 const currentHour = moment().format("H");
+//const currentHour = 9;
+var textAreaArray = [];
+var textAreaObj = {
+    name: "",
+    text: ""
+};
+
+//populateTextAreas();
 
 $("#currentDay").text(currentDate);
 
@@ -16,12 +24,11 @@ function updateTime() {
 
 
 for (i = 0; i < $(".text-box").length; i++) {
-    var currentTextBox = $(".text-box")[i];
+    const currentTextBox = $(".text-box")[i];
 
     if (parseInt(currentTextBox.dataset.value) < parseInt(currentHour)) {
         currentTextBox.classList.add("past");
         currentTextBox.setAttribute("disabled", "");
-        console.log(currentTextBox);
     }
     else if ((parseInt(currentTextBox.dataset.value)) === parseInt(currentHour)) {
         currentTextBox.classList.add("present");
@@ -31,7 +38,36 @@ for (i = 0; i < $(".text-box").length; i++) {
     }
 }
 
+function populateTextAreas() {
+    const textAreaLocalStorage = JSON.parse(localStorage.getItem("textAreaArray"));
+    if (textAreaLocalStorage !== null) {
+        textAreaArray = textAreaLocalStorage;
+        
+        for (i = 0; i < textAreaArray.length; i++) {
+            if(textAreaArray[i].name === textAreaObj.name) {
+                textAreaArray[i].text = textAreaObj.text;
+            }
+        }
+        
+        if(textAreaArray.some(e => e.name === textAreaObj.name) === false) {
+            textAreaArray.push(textAreaObj);
+        }
+    }
+    else {
+        textAreaArray.push(textAreaObj);
+    }
+    localStorage.setItem("textAreaArray", JSON.stringify(textAreaArray));
+}
 
+$(".saveBtn").on("click", function(e) {
+    e.preventDefault();
+    
+    const textAreaInfo = this.previousElementSibling.value;
+    textAreaObj.name = this.previousElementSibling.id;
+    textAreaObj.text = textAreaInfo;
+
+    populateTextAreas();
+});
 
 
 
