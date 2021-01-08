25e84694118fef3388395d61ec41121f4dfd7ec9
Kolton Decker <deckerman25@gmail.com>
2021-01-07 23:12:23 -0600

    Updated data id values for time blocks to fit 24 hour clock. Added text area color updating based on current time functionality.

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,11 +2,11 @@
 $(document).ready(function() {
 
 const currentDate = moment().format("dddd, MMMM Do, YYYY");
-
-
-
+const currentHour = moment().format("H");
 
 $("#currentDay").text(currentDate);
+
+//Appends time to page continually in real time
 updateTime();
 function updateTime() {
     const currentTime = moment().format("h:mm:ss a");
@@ -15,6 +15,19 @@ function updateTime() {
 }
 
 
+for (i = 0; i < $(".text-box").length; i++) {
+    var currentTextBox = $(".text-box")[i];
+
+    if (parseInt(currentTextBox.dataset.value) < parseInt(currentHour)) {
+        currentTextBox.classList.add("past");
+    }
+    else if ((parseInt(currentTextBox.dataset.value)) === parseInt(currentHour)) {
+        currentTextBox.classList.add("present");
+    }
+    else {
+        currentTextBox.classList.add("future");
+    }
+}
 
 
 
