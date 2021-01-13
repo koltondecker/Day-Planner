6b225fc896ef5ba50b6e89fbc5e6104c513269a9
Kolton Decker <deckerman25@gmail.com>
2021-01-12 18:53:41 -0600

    Added comments to javascript file to explain functionality.

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,18 +5,20 @@ $(document).ready(function() {
     var initialHour = moment().format("H");
     var curHourCheck = initialHour;
 
+    //Declaring empty array and empty object for textbox areas to append to localstorage.
     var textAreaArray = [];
     var textAreaObj = {
         name: "",
         text: ""
     };
 
+    //Initially call our functions to populate text boxes from local storage and update text box colors to current time.
     populateTextAreas();
     updateTextBoxColors(initialHour);
 
     $("#currentDay").text(currentDate);
 
-    //Appends time to page continually in real time
+    //Appends time to page continually in real time. Function repeats every second.
     updateTime();
     function updateTime() {
         const currentTime = moment().format("h:mm:ss a");
@@ -24,6 +26,7 @@ $(document).ready(function() {
 
         $("#currentTime").text(currentTime);
 
+        //Checks if time rolls over to a new hour and runs the change hour function.
         if (currentHour > curHourCheck) {
             changeHour();
             curHourCheck = currentHour;
@@ -31,6 +34,7 @@ $(document).ready(function() {
         setTimeout(updateTime, 1000);
     }
 
+    //Function uses current hour to compare against hour dataset value of all text boxes and changes css class appropriately.
     function updateTextBoxColors(hour) {
         for (i = 0; i < $(".text-box").length; i++) {
             const currentTextBox = $(".text-box")[i];
@@ -48,6 +52,7 @@ $(document).ready(function() {
         }
     }
 
+    //Function takes text box that matches current hour and the next text box and changes css classes. 
     function changeHour() {
         for (i = 0; i < $(".text-box").length; i++) {
             if ($(".text-box")[i].classList.contains("present")) {
@@ -61,6 +66,8 @@ $(document).ready(function() {
         }
     }
 
+    //Function parses local storage for array that contains values saved in the day planner and appends back to page. 
+    //Also takes in new values when save button is pressed and adds to array in local storage.
     function populateTextAreas() {
         var textAreaLocalStorage = JSON.parse(localStorage.getItem("textAreaArray"));
         if (textAreaLocalStorage !== null) {
@@ -73,7 +80,9 @@ $(document).ready(function() {
             }
             
             if(textAreaArray.some(e => e.name === textAreaObj.name) === false) {
-                textAreaArray.push(textAreaObj);
+                if ( textAreaObj.name !== "" && textAreaObj.text !== "") {
+                    textAreaArray.push(textAreaObj);
+                }
             }
         }
         else {
@@ -92,6 +101,8 @@ $(document).ready(function() {
         }
     }
 
+    //Listens for a save button to be pressed and uses text object to update hour name and hour value and call populateTextAreas function.
+    //This will keep hour blocks better organized in local storage array by using objects.
     $(".saveBtn").on("click", function(e) {
         e.preventDefault();
         
@@ -102,6 +113,7 @@ $(document).ready(function() {
         populateTextAreas();
     });
 
+    //Clears all text boxes on page and clears local storage array then reloads the page.
     $("#clear-button").on("click", function() {
         localStorage.removeItem("textAreaArray");
         $(".text-box").value = "";
